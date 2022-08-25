323e2ddb637bffcbd8570fb4e649e7b5634f8bc0
AyrielNoriega <anoriega2310@gmail.com>
2022-08-24 19:03:15 -0500

    config controller

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -4,6 +4,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Libro;
 
-class LibroController extends Controller{
+class LibroController extends Controller
+{
+    public function index(){
+
+        $datosLibro = Libro::all();
+        return response()->json($datosLibro);
+    }
+
+    public function store(Request $request)
+    {
+        $libro = new Libro;
+        $libro->titulo  = $request->titulo;
+        $libro->archivo = $request->archivo;
+        $libro->save();
+
+        return response()->json($request);
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/libros', 'LibroController@index');
+$router->post('/libros', 'LibroController@store');
